90063ca9cb9a0ee4d8a8338509c3d25dbf5e49a9
Vaibhavi-Apps <vppanchalofficial@gmail.com>
2021-12-12 16:48:22 +0530

    Task Create with bottom sheet

--- app/src/main/java/com/vppanchalofficial/reminder/HomeActivity.kt
+++ app/src/main/java/com/vppanchalofficial/reminder/HomeActivity.kt
@@ -3,6 +3,9 @@ package com.vppanchalofficial.reminder
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
@@ -10,15 +13,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.vppanchalofficial.reminder.databinding.ActivityHomeBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var dao: TaskDao
     lateinit var binding: ActivityHomeBinding
     private var adapter = myTaskListAdapter(this)
 
+    private lateinit var repository: TaskRepository
+    private lateinit var mainViewModelV: mainViewModel
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
@@ -29,11 +37,11 @@ class HomeActivity : AppCompatActivity() {
         binding.taskList.setLayoutManager(staggeredGridLayoutManager)
         binding.taskList.adapter = adapter
 
-        val dao = TaskAppDataBase.getDatabase(applicationContext).taskDao()
-        val repository = TaskRepository(dao)
-        val mainViewModel = ViewModelProvider(this, MainViewModelFectory(repository)).get(mainViewModel::class.java)
+        dao = TaskAppDataBase.getDatabase(applicationContext).taskDao()
+        repository = TaskRepository(dao)
+        mainViewModelV = ViewModelProvider(this, MainViewModelFectory(repository)).get(mainViewModel::class.java)
 
-        mainViewModel.getTask().observe(this, {
+        mainViewModelV.getTask().observe(this, {
             adapter.setList(it)
         })
 
@@ -43,8 +51,42 @@ class HomeActivity : AppCompatActivity() {
     }
 
     fun openActivty(view: android.view.View) {
-        val intent = Intent(this, CreateActivity::class.java)
-        startActivity(intent)
+        /*val intent = Intent(this, CreateActivity::class.java)
+        startActivity(intent)*/
+
+        val bottomsheet = BottomSheetDialog(this)
+        bottomsheet.getWindow()?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        val views = layoutInflater.inflate(R.layout.activity_create, null)
+        bottomsheet.setContentView(views)
+
+        val saveButton = views.findViewById<Button>(R.id.save_task)
+        val taskName = views.findViewById<EditText>(R.id.task)
+        val taskDescription = views.findViewById<EditText>(R.id.text2)
+        bottomsheet.show()
+
+        saveButton.setOnClickListener{
+            if(taskName.text ==  null || taskName.text.toString().equals("")){
+                taskName.error = "Please Enter Task"
+            }else {
+                bottomsheet.dismiss()
+                GlobalScope.launch {
+                    mainViewModelV.insertTask(TaskModel(
+                        0,
+                        taskName.text.toString(),
+                        taskDescription.text.toString()
+                    ))
+
+                    /*   database.taskDao().insertTask(
+                           TaskModel(
+                               0,
+                               taskName.text.toString(),
+                               binding.text2.text.toString()
+                           )
+                       )*/
+                }
+            }
+        }
+
     }
 
     override fun onRestart() {
